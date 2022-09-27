dc263cd1a0ac4187c545f0aef04ce4990358b781
Emem Udofia <ememudofia456@gmail.com>
2022-09-27 15:09:21 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""module to load and save"""
+"""This module adds all arguments to a Python list and save them to a file."""
+
+
 import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file =\
+    load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
     try:
